b747bec85c6dc5099a5d7fd6d2b73d8dadf3ad44
allissondantas <allissondantas@gmail.com>
2020-05-11 16:39:08 -0300

    informe_may11

--- risk_diagrams.py
+++ risk_diagrams.py
@@ -3,7 +3,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from matplotlib.colors import ListedColormap
 from matplotlib.backends.backend_pdf import PdfPages
+from pandas import ExcelWriter
 from crear_excel_brasil import run_crear_excel_brasil
 
 def gradient_image(ax, extent, direction=0.3, cmap_range=(0, 1), **kwargs):
@@ -34,7 +36,7 @@ def gradient_image(ax, extent, direction=0.3, cmap_range=(0, 1), **kwargs):
                   [v @ [0, 0], v @ [0, 1]]])
     a, b = cmap_range
     X = a + (b - a) / X.max() * X
-    im = ax.imshow(X, extent=extent, alpha=0.4, interpolation='bicubic',
+    im = ax.imshow(X, extent=extent, alpha=0.45, interpolation='bicubic',
                    vmin=0, vmax=1, **kwargs)
     return im
 
@@ -92,7 +94,7 @@ def main():
                 a_14_days[i] = n_14_days[i] / pop * 100000
                 risk[i] = n_14_days[i]  * p_seven[i]
                 risk_per_10[i] = a_14_days[i]  * p_seven[i]
-                print(dia[i], cumulative_cases[i], new_cases[i], p[i], p_seven[i], n_14_days[i], a_14_days[i], risk[i], risk_per_10[i])
+                #print(dia[i], cumulative_cases[i], new_cases[i], p[i], p_seven[i], n_14_days[i], a_14_days[i], risk[i], risk_per_10[i])
             first_day = dia[13]
             last_day = dia[len(dia) - 1]
             first_day = first_day.replace('/','-')
@@ -107,19 +109,19 @@ def main():
                 lim = ax1.get_xlim()
                 x = np.ones(int(lim[1]))
                 ax1.plot(x, 'k-', fillstyle='none', linewidth=0.5)
-                #ax1.set_ylim(0, 4)
+                ax1.set_ylim(0, 4)
                 #ax1.set_xlim(0, len(x))
                 ax1.set_ylabel('$\u03C1$ (mean of the last 7 days)')
                 ax1.set_xlabel('Attack rate per $10^5$ inh. (last 14 days)')
                 ax1.annotate(first_day,
                                 xy=(a_14_days[13], p_seven[13]), xycoords='data',
-                                xytext=(15, 3.2), textcoords='data',
+                                xytext=(len(x) - 30, 3), textcoords='data',
                                 arrowprops=dict(arrowstyle="->",
                                                 connectionstyle="arc3", linewidth=0.4),
             )
                 ax1.annotate(last_day,
                                 xy=(a_14_days[len(a_14_days) - 1], p_seven[len(p_seven) - 1]), xycoords='data',
-                                xytext=(30, 2.8), textcoords='data',
+                                xytext=(len(x) - 10, 3.5), textcoords='data',
                                 arrowprops=dict(arrowstyle="->",
                                                 connectionstyle="arc3", linewidth=0.4),
             )
@@ -130,9 +132,10 @@ def main():
                     plt.title(bra_title)
                 else:
                     plt.title(Region[ID])
-                    
-                gradient_image(ax1, direction=0.3, extent=(0, 1, 0, 1), transform=ax1.transAxes,
-                cmap=plt.cm.nipy_spectral, cmap_range=(0.45, 0.9))
+                color_map = plt.cm.hsv
+                cmap = color_map.reversed()
+                gradient_image(ax1, direction=0.4, extent=(0, 1, 0, 1), transform=ax1.transAxes,
+                cmap=cmap, cmap_range=(0.6, 1.1))
                 ax1.set_aspect('auto')
                 fig1.tight_layout()
                 #plt.show()
@@ -143,6 +146,23 @@ def main():
                     print("\n\nPrediction for the region of "+Region[ID]+" performed successfully!\nPath:" + savePath)
                 except:
                     print("An exception occurred")
+            '''
+            df = pd.DataFrame({
+                                'Day': DIA,
+                                'Cumulative cases': cumulative_cases,
+                                'New cases': new_cases,
+                                'ρ': p,
+                                'ρ7': p_seven,
+                                'New cases last 14 days (N14)': n_14_days,
+                                'New cases last 14 days per 105 inhabitants (A14)': a_14_days,
+                                'Risk (N14*ρ7)': risk,
+                                'Risk per 105 (A14*ρ7)': risk_per_10,
+                                'population':pop 
+      
+            })
+            with ExcelWriter('/home/allissondantas/Matlab/Code/Indexs_Brasil-sp.xlsx') as writer:
+                df.to_excel(writer, sheet_name='Alt_Urgell')
+            '''
             #break
 if __name__ == "__main__":
     #sys.argv.append('brasil')
